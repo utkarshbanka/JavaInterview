fa6055776d7099383eba7a4be5a1316dadb164b5
=utkarshbanka <utkarsh.k.keshari@gmail.com>
2026-04-14 18:34:43 +0530

    Intial-Commit

---
+++ src/JavaJreJvm/Question1.java
@@ -0,0 +1,27 @@
+package JavaJreJvm;
+
+public class Question1 {
+
+
+    //    Explain the JVM architecture and its main components (Class Loader, Runtime
+//            Data Areas, Execution Engine). What are the different runtime data areas in JVM
+//            (Heap, Stack, Metaspace, Code Cache)?
+
+
+     //heap , stack, metasapce, code cache
+     //heap  heap is memory where object data is sore
+    // stack = stack is the memory where object refecrec method call store
+    // MetaSpace = Metasapce is place Where class details isntance varaible and method variable is store
+     // code cache = Place where code cache store used by by the jit compiler
+      ///  jit just in ti
+
+     // Java Virutal Machine is a Runtime place Where Java Code Executes
+
+      // You Write Java Code compile its .class(bytecode) jvm exuite krta hey
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
